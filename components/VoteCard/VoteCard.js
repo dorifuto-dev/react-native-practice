00b630c99ee2f4b9fece8abc1caad55ac61c575b
Eric Li <ericli@knights.ucf.edu>
2022-01-27 19:13:48 -0700

    Add basic styling

--- components/VoteCard/VoteCard.js
+++ components/VoteCard/VoteCard.js
@@ -1,24 +1,49 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, Button } from 'react-native'
 
 const VoteCard = ({ songInfo, incrementVote, totalVotes }) => {
 
   let { songName, artist, numVotes, voteId } = songInfo;
 
+  const percentage = totalVotes ? ((numVotes / totalVotes) * 100).toFixed(1) : 0
+
   return (
-    <View>
-      <Text>
-        {songName}      
-      </Text>
-      <Text>
-        {numVotes}
-      </Text>
-      <Text>
-        {totalVotes}
-      </Text>
-      <Button 
-        title="Clicky"
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: "7px%",
+        backgroundColor: "#f5f5f5",
+        position: "relative",
+        borderRadius: 10,
+        margin: 10
+      }}
+    >
+      <View>
+        <Text
+          style={{
+            fontWeight: "700"
+          }}
+        >
+          {songName}      
+        </Text>
+        <Text>
+          {artist}
+        </Text>
+        <Text>
+          {`${numVotes} votes`}
+        </Text>
+        <Text>
+          {`${percentage}%`}
+        </Text>
+      </View>
+      <Button
+        styles={{
+        }}
+        title="Vote"
         onPress={() => incrementVote(voteId)}
       />
     </View>
